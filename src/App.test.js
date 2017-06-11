0dca950a274a8ea7e378a925cf303794ac7de60f
Nick Tsourektsidis <creatorgr@hotmail.com>
2017-06-11 22:29:33 +0300

    Install react-test-renderer. Add first snapshot for app.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders correctly (useing snapshot)', () => {
+  const tree = renderer.create(
+    <App />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
