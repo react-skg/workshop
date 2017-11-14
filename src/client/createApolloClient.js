c88a476a47ef0ade8594e91d049f9beedbba2361
James Gatz <jamesgatz@Jamess-MacBook-Pro.local>
2017-11-14 22:12:13 +0200

    chore(apollo): Create apollo client

---
+++ src/client/createApolloClient.js
@@ -0,0 +1,15 @@
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+/**
+ * Creates a new ApolloClient
+ */
+const createApolloClient = () => {
+  return new ApolloClient({
+    link: createHttpLink({ uri: 'https://8lnxkljvq.lp.gql.zone/graphql' }),
+    cache: new InMemoryCache()
+  });
+};
+
+export default createApolloClient;
