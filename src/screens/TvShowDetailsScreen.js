54516140f2d46bb870028f27698e2a3f7fd99339
James Gatz <jamesgatz@Jamess-MacBook-Pro.local>
2017-11-14 22:57:03 +0200

    feat(adopt-apollo): Use apollo to get the tvShow

--- src/screens/TvShowDetailsScreen.js
+++ src/screens/TvShowDetailsScreen.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-
+import { graphql, compose } from 'react-apollo';
+import gql from 'graphql-tag';
 import { fetchShowSuccess, fetchTrailerSuccess } from '../state/actions';
 import TvShowDetails from '../components/TvShows/TvShowDetails/TvShowDetails';
 
@@ -11,30 +12,37 @@ const { string } = PropTypes;
 
 class TvShowDetailsScreen extends Component {
   componentDidMount() {
-    fetch(`https://api.themoviedb.org/3/tv/${this.props.match.params.id}?api_key=ccd7f7c8bc4f625411a4e4925c0c5931`)
-      .then((response) => {
-        return response.text()
-      }).then((body) => {
-        const data = JSON.parse(body);
-        this.props.onFetchSuccess(data);
-      })
+    // fetch(
+    //   `https://api.themoviedb.org/3/tv/${this.props.match.params
+    //     .id}?api_key=ccd7f7c8bc4f625411a4e4925c0c5931`
+    // )
+    //   .then(response => {
+    //     return response.text();
+    //   })
+    //   .then(body => {
+    //     const data = JSON.parse(body);
+    //     this.props.onFetchSuccess(data);
+    //   });
 
-    fetch(`https://api.themoviedb.org/3/tv/${this.props.match.params.id}/videos?api_key=ccd7f7c8bc4f625411a4e4925c0c5931`)
-      .then((response) => {
-        return response.text()
-      }).then((body) => {
+    fetch(
+      `https://api.themoviedb.org/3/tv/${this.props.match.params
+        .id}/videos?api_key=ccd7f7c8bc4f625411a4e4925c0c5931`
+    )
+      .then(response => {
+        return response.text();
+      })
+      .then(body => {
         const data = JSON.parse(body);
         if (data.results && data.results.length > 0) {
           this.props.onFetchTrailerSuccess(data.results[0].key);
         } else {
           this.props.onFetchTrailerSuccess('');
         }
-
-      })
+      });
   }
 
   handleChange(e) {
-    this.setState({ searchFieldValue: e.target.value })
+    this.setState({ searchFieldValue: e.target.value });
   }
 
   render() {
@@ -60,11 +68,11 @@ class TvShowDetailsScreen extends Component {
 
 TvShowDetailsScreen.defaultProps = {
   trailerId: string.isRequired
-}
+};
 // Container
 
 const mapStateToProps = state => ({
-  show: state.show,
+  // show: state.show,
   trailerId: state.trailerId
 });
 
@@ -73,4 +81,38 @@ const mapDispatchToProps = dispatch => ({
   onFetchTrailerSuccess: data => dispatch(fetchTrailerSuccess(data))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(TvShowDetailsScreen);
+// The graphql Query to get the TvShow
+const tvShowQuery = gql`
+  query getTvShow($id: ID!) {
+    tvShow(id: $id) {
+      id
+      name
+      backdrop_path
+      overview
+      vote_average
+      poster_path
+      first_air_date
+      genres {
+        name
+      }
+    }
+  }
+`;
+
+// React-Apollo Container.
+// Responsible for passing down the data from apollo
+const TvShowDataContainer = graphql(tvShowQuery, {
+  props: ({ data }) => ({
+    show: data.tvShow || {}
+  }),
+  options: ownProps => ({
+    variables: {
+      id: ownProps.match.params.id
+    }
+  })
+});
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  TvShowDataContainer
+)(TvShowDetailsScreen);
