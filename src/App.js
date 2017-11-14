2d21e2db391984ff0d1adf78dbc9b7f163f035e7
James Gatz <jamesgatz@Jamess-MacBook-Pro.local>
2017-11-15 00:39:10 +0200

    refactor(remove-redux): Remove redux 🔥

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 import createApolloClient from './client';
-import { store } from './state/store';
 import TvShowsScreen from './screens/TvShowsScreen';
 import TvShowDetailsScreen from './screens/TvShowDetailsScreen';
 
@@ -11,14 +9,12 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={createApolloClient()}>
-        <Provider store={store}>
           <Router>
             <div>
               <Route path="/" exact component={TvShowsScreen} />
               <Route path="/show/:id" component={TvShowDetailsScreen} />
             </div>
           </Router>
-        </Provider>
       </ApolloProvider>
     );
   }
--- src/state/store.js
+++
@@ -1,4 +0,0 @@
-import { createStore } from 'redux';
-import { reducer as tvShows } from './reducer';
-
-export const store = createStore(tvShows);
